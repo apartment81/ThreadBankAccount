5a5c80bc257bd08b44d3e524f5c32d462418b174
apartment81 <bogdan.mirodone@gmail.com>
2019-02-06 19:20:04 +0200

    ReentrantLock

--- src/ro/mirodone/BankAccount.java
+++ src/ro/mirodone/BankAccount.java
@@ -1,33 +1,40 @@
 package ro.mirodone;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
     private double balance;
     private String accountNumber;
+    private Lock lock;
 
-    public BankAccount(double ibalance, String accountNumber) {
-        this.balance = ibalance;
+    public BankAccount(double initialBalance, String accountNumber) {
+        this.balance = initialBalance;
         this.accountNumber = accountNumber;
+        this.lock = new ReentrantLock();
     }
 
- // option 1
-/*    public synchronized void deposit (double amount){
-        balance += amount;
-    }
+    public void deposit(double amount) {
 
-    public synchronized void withdraw (double amount){
-        balance -= amount;
-    }*/
-
-    public  void deposit (double amount){
-        synchronized (this){
+        lock.lock();
+        try {
             balance += amount;
+        } finally {
+            {
+                lock.unlock();
+            }
         }
     }
 
-    public  void withdraw (double amount) {
-        synchronized (this) {
+    public void withdraw(double amount) {
+        lock.lock();
+        try {
             balance -= amount;
+        } finally {
+            {
+                lock.unlock();
+            }
         }
     }
 
--- src/ro/mirodone/Main.java
+++ src/ro/mirodone/Main.java
@@ -7,23 +7,6 @@ public class Main {
 
         BankAccount account = new BankAccount(1000, "2222");
 
-        //option 1
-       /* Thread th1 = new Thread() {
-
-            public void run() {
-                account.deposit(300);
-                account.withdraw(50);
-
-            }
-        };
-
-        Thread th2 = new Thread() {
-            public void run() {
-                account.deposit(200);
-                account.withdraw(100);
-                System.out.println("sold final " + account.getBalance());
-            }
-        };*/
 
        Thread th1 = new Thread(new Runnable() {
            @Override
